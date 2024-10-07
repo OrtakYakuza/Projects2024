55fd5de6de400ba375378e7d483363b2d0b67b6b
Zara Ahmed <zara.ahmed@uzh.ch>
2024-10-07 19:50:31 +0200

    removed parent class as it is alr in main

--- SoCo-hs24-group-55-a1/vacation_booking.py
+++ SoCo-hs24-group-55-a1/vacation_booking.py
@@ -1,25 +1,3 @@
-import math
-
-# abstract methods of the parent class
-
-def calculate_cost(thing):
-    raise NotImplementedError("Method has not been Implemented")
-
-def describe_package(thing):
-    raise NotImplementedError("Method has not been Implemented")
-
-# parent class
-
-VacationPackage = {
-    "destination" : None,
-    "cost_per_day" : None,
-    "duration_in_days" : None,
-    "calculate_cost" : calculate_cost,
-    "describe_package" : describe_package,
-    "_classname" : "VacationPackage",
-    "_parent": None,
-}
-
 def calculate_cost(thing):
     if thing["includes_surfing"] == True:
         return thing["cost_per_day"] * thing["duration_in_days"] + 100
