591070e75fb226c63ab2c7b117a5d259717fde05
Stevania Frutiger <stevaniaeilyn.frutiger@uzh.ch>
2024-10-07 20:57:35 +0200

    Update vacation_booking.py

--- SoCo-hs24-group-55-a1/vacation_booking.py
+++ SoCo-hs24-group-55-a1/vacation_booking.py
@@ -1,25 +1,3 @@
-import math
-
-# abstract methods of the parent class
-
-def calculate_cost(thing):
-    raise NotImplementedError("Method has not been Implemented")
-
-def describe_package(thing):
-    raise NotImplementedError("Method has not been Implemented")
-
-# parent class
-
-VacationPackage = {
-    "destination" : None,
-    "cost_per_day" : None,
-    "duration_in_days" : None,
-    "calculate_cost" : calculate_cost,
-    "duration_in_days" : duration_in_days,
-    "_classname" : "VacationPackage",
-    "_parent": None,
-}
-
 # make function
 
 def make(package, destination, cost_per_day, duration_in_days, *args):
