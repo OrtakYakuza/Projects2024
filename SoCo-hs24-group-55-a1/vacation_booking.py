9f48908764669e076a65611ade72b087a6acaf0f
Zara Ahmed <zara.ahmed@uzh.ch>
2024-10-07 20:45:56 +0200

    Update file vacation_booking.py

--- SoCo-hs24-group-55-a1/vacation_booking.py
+++ SoCo-hs24-group-55-a1/vacation_booking.py
@@ -5,14 +5,14 @@ def beach_resort_calculate_cost(thing):
 
 def beach_resort_describe_package(thing):
     if thing["includes_surfing"] == True:
-        return f"The thing['duration_in_days'] day long Beach Resort vacation in thing['destination'] includes surfing."
-    return f"The thing['duration_in_days'] day long Beach Resort vacation in thing['destination'] does not include surfing."  
+        return f"The {thing['duration_in_days']} day long Beach Resort vacation in {thing['destination']} includes surfing."
+    return f"The {thing['duration_in_days']} day long Beach Resort vacation in {thing['destination']} does not include surfing."  
 
                   
 Beach_Resort = {
     "includes_surfing": None,
-    "calculate cost" : beach_resort_calculate_cost
-    "describe package" : beach_resort_describe_package
+    "calculate cost" : beach_resort_calculate_cost,
+    "describe package" : beach_resort_describe_package,
     "_classname":"BeachResort",
     "_parent":VacationPackage
 }
