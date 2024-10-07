1cefd177378ea22045ab0899ce3b0f398bdeb1e2
Zara Ahmed <zara.ahmed@uzh.ch>
2024-10-07 21:40:34 +0200

    Update vacation_booking.py

--- SoCo-hs24-group-55-a1/vacation_booking.py
+++ SoCo-hs24-group-55-a1/vacation_booking.py
@@ -2,7 +2,14 @@
 
 def make(package, destination, cost_per_day, duration_in_days, *args):
     if package == BeachResort:
-        pass
+        includes_surfing = args[0]
+        new_thing = {
+            "destination" : destination,
+            "cost_per_day" : cost_per_day,
+            "duration_in_days" : duration_in_days,
+            "includes_surfing" : includes_surfing,
+            "_class" : BeachResort}
+        return new_thing
     elif package == AdventureTrip:
         difficulty_level = args[0]
         new_thing = {
