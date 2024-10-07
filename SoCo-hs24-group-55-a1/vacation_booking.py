8323b12847d961ea91fe850cf2f66e667d0d5c01
Zara Ahmed <zara.ahmed@uzh.ch>
2024-10-07 19:33:58 +0200

    final change

--- SoCo-hs24-group-55-a1/vacation_booking.py
+++ SoCo-hs24-group-55-a1/vacation_booking.py
@@ -21,25 +21,9 @@ VacationPackage = {
 }
 
 def calculate_cost(thing):
-    raise NotImplementedError("Method has not been Implemented")
-
-def describe_package(thing):
-    raise NotImplementedError("Method has not been Implemented")
-
-
-VacationPackage = {
-    "destination" : None,
-    "cost_per_day" : None,
-    "duration_in_days" : None,
-    "calculate_cost" : calculate_cost,
-    "_classname" : "VacationPackage",
-    "_parent": None,
-}
-
-def calculate_cost(thing):
     if thing["includes_surfing"] == True:
         return BeachResort["cost_per_day"] * thing["duration_in_days"] + 100
-    thing["cost_per_day"] * thing["duration_in_days"]
+    return thing["cost_per_day"] * thing["duration_in_days"]
 
 def describe_package(thing):
     if thing["includes_surfing"] == True:
