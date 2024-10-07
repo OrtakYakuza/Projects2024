0492190cee22767ae49fa2ab19f3a4dea4fb4428
Stevania Frutiger <stevaniaeilyn.frutiger@uzh.ch>
2024-10-07 17:52:39 +0200

    renaming

--- SoCo-hs24-group-55-a1/vacation_booking.py
+++ SoCo-hs24-group-55-a1/vacation_booking.py
@@ -22,17 +22,18 @@ VacationPackage = {
 
 # AdventureTrip
 
-def calculate_cost(thing):
+def adventure_trip_calculate_cost(thing):
     if thing["difficulty_level"] == "hard":
         return thing["cost_per_day"] * thing["duration_in_days"] * 2
     return thing["cost_per_day"] * thing["duration_in_days"]
 
-def describe_package(thing):
+def adventure_trip_describe_package(thing):
     return f"The {thing['duration_in_days']} day long Adventure Trip in {thing['destination']} is considered {thing['difficulty_level']}."
 
 AdventureTrip = {
-    "difficulty_level" : None
+    "difficulty_level" : None,
+    "calculate_cost" : adventure_trip_calculate_cost,
+    "describe_package" : adventure_trip_describe_package,
     "_classname" : "AdventureTrip",
-    "_parent" : VacationPackage
-
+    "_parent" : VacationPackage,
 }
