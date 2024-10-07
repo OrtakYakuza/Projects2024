9afb700d8d5f64f27f0241aa19cf7a26eeba6ab5
Zara Ahmed <zara.ahmed@uzh.ch>
2024-10-07 19:32:02 +0200

    Beach_Resort Class

--- SoCo-hs24-group-55-a1/vacation_booking.py
+++ SoCo-hs24-group-55-a1/vacation_booking.py
@@ -19,3 +19,39 @@ VacationPackage = {
     "_classname" : "VacationPackage",
     "_parent": None,
 }
+
+import math
+
+
+def calculate_cost(thing):
+    raise NotImplementedError("Method has not been Implemented")
+
+def describe_package(thing):
+    raise NotImplementedError("Method has not been Implemented")
+
+
+VacationPackage = {
+    "destination" : None,
+    "cost_per_day" : None,
+    "duration_in_days" : None,
+    "calculate_cost" : calculate_cost,
+    "_classname" : "VacationPackage",
+    "_parent": None,
+}
+
+def calculate_cost(thing):
+    if thing["includes_surfing"] == True:
+        return BeachResort["cost_per_day"] * thing["duration_in_days"] + 100
+    thing["cost_per_day"] * thing["duration_in_days"]
+
+def describe_package(thing):
+    if thing["includes_surfing"] == True:
+        return f"The thing['duration_in_days'] day long Beach Resort vacation in thing['destination'] includes surfing."
+    return f"The thing['duration_in_days'] day long Beach Resort vacation in thing['destination'] does not include surfing."  
+
+                  
+Beach_Resort = {
+    "includes_surfing": None,
+    "_classname":"BeachResort",
+    "_parent":VacationPackage
+}
