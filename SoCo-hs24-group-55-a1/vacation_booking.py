f02a235a294cbca228e0426625e7e1c75d660c8a
Stevania Frutiger <stevaniaeilyn.frutiger@uzh.ch>
2024-10-07 16:29:07 +0200

    parent class

--- SoCo-hs24-group-55-a1/vacation_booking.py
+++ SoCo-hs24-group-55-a1/vacation_booking.py
@@ -1,4 +1,21 @@
 import math
 
+# abstract methods of the parent class
+
+def calculate_cost(thing):
+    raise NotImplementedError("Method has not been Implemented")
+
+def duration_in_days(thing):
+    raise NotImplementedError("Method has not been Implemented")
+
+# parent class
+
 VacationPackage = {
+    "destination" : None,
+    "cost_per_day" : None,
+    "duration_in_days" : None,
+    "calculate_cost" : calculate_cost(),
+    "duration_in_days" : duration_in_days(),
+    "_classname" : "VacationPackage",
+    "_parent": None,
 }
